70ab1b5d82c3d04307e866f4d7be3165a749f3c4
Nick van Dyke <nickjvandyke15@gmail.com>
2026-05-24 23:27:26 -0600

    use `:mark` for consistency and clarity

--- lua/jump/init.lua
+++ lua/jump/init.lua
@@ -96,7 +96,7 @@ function M.start()
       end
 
       if jump_to then
-        vim.cmd("normal! m'")
+        vim.cmd("mark '")
         api.nvim_win_set_cursor(win, jump_to)
         vim.cmd('foldopen')
       end
@@ -105,7 +105,7 @@ function M.start()
     elseif char == BS or char == CTRL_H then
       chars = chars:sub(1, #chars - 1)
     elseif jump_to then
-      vim.cmd("normal! m'")
+      vim.cmd("mark '")
       api.nvim_win_set_cursor(win, jump_to)
       vim.cmd('foldopen')
       break
